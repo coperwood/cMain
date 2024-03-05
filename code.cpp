85a8fa5cedfefc557833838bc310d3f20047cf4c
Josh Carolin <162287200+coperwood@users.noreply.github.com>
2024-03-05 09:29:16 +0200

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,22 @@
+int Model::nverts() const {
+    return verts.size();
+}
+
+int Model::nfaces() const {
+    return facet_vrt.size()/3;
+}
+
+vec3 Model::vert(const int i) const {
+    return verts[i];
+}
+
+vec3 Model::vert(const int iface, const int nthvert) const {
+    return verts[facet_vrt[iface*3+nthvert]];
+}
+
+void Model::load_texture(std::string filename, const std::string suffix, TGAImage &img) {
+    size_t dot = filename.find_last_of(".");
+    if (dot==std::string::npos) return;
+    std::string texfile = filename.substr(0,dot) + suffix;
+    std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
+}
