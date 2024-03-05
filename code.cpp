c94ee67eca92678b3ace99b5891066a67cbedefd
Josh Carolin <162287200+coperwood@users.noreply.github.com>
2024-03-05 09:29:45 +0200

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -21,3 +21,7 @@ void Model::load_texture(std::string filename, const std::string suffix, TGAImag
     std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
 }
 //nice
+vec3 Model::normal(const vec2 &uvf) const {
+    TGAColor c = normalmap.get(uvf[0]*normalmap.width(), uvf[1]*normalmap.height());
+    return vec3{(double)c[2],(double)c[1],(double)c[0]}*2./255. - vec3{1,1,1};
+}
